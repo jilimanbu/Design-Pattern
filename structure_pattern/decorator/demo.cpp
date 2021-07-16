db764e89325031b586eed277616ad2ea7741ab91
jilimanbu <1434521355@qq.com>
2021-07-17 07:32:19 +0800

    decorator 2021-07-17

---
+++ structure_pattern/decorator/demo.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+ 
+using std::string;
+using std::cout;
+
+class Component{
+public:
+    virtual ~Component(){};
+    virtual string Operation() const = 0;
+};
+
+class ConcreteCpmponent : public Component{
+public:
+    string Operation() const override{
+        return "ConcreteComponent";
+    }
+};
+
+class Decorator : public Component{
+public:
+    string Operation() const override{
+        return this->component_->Operation();
+    }
+    Decorator(Component* component):component_(component){
+    }
+protected:
+    Component* component_;
+};
+
+class ConcreteDecoratorA:public Decorator{
+public:
+    ConcreteDecoratorA(Component* component):Decorator(component){
+    }
+    string Operation() const override{
+        return "ConcreteDecoratorA(" + Decorator::Operation() + ")";
+    }
+};
+
+class ConcreteDecoratorB:public Decorator{
+public:
+    ConcreteDecoratorB(Component* component):Decorator(component){
+    }
+    string Operation() const override{
+        return "ConcreteDecoratorB(" + Decorator::Operation() + ")";
+    }
+};
+
+void ClientCode(Component* component){
+    cout << "RESULT: " << component->Operation();
+}
+
+int main(){
+    Component* simple = new ConcreteCpmponent();
+    cout << "Client: I've got a simple component:\n";
+    ClientCode(simple);
+    cout << "\n\n";
+    
+    Component* decorator1 = new ConcreteDecoratorA(simple);
+    Component* decorator2 = new ConcreteDecoratorB(decorator1);
+    cout << "Client: Now I've got a decorated component:\n";
+    ClientCode(decorator2);
+    std::cout << "\n";
+
+    delete simple;
+    delete decorator1;
+    delete decorator2;
+
+    system("pause");
+    return 0;
+}
